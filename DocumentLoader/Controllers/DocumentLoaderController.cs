639509ee6e97e434cce21aa56d938759d0b381d9
Edmund Goller <matrim.eg@gmail.com>
2019-10-06 10:05:21 +0200

    Removed boilerplate

---
+++ DocumentLoader/Controllers/DocumentLoaderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DocumentLoader.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentLoaderController : ControllerBase
+    {
+        private readonly ILogger<DocumentLoaderController> _logger;
+
+        public DocumentLoaderController(ILogger<DocumentLoaderController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
---
+++ DocumentLoader/Document.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace DocumentLoader
+{
+    public class Document
+    {
+        public DateTime Date { get; set; }
+
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+
+        public string Path { get; set; }
+    }
+}
